74dd2644b3d30c2ff99ce866b74ea805d1ede6d3
Dewei Xiang <dewei.xiang@plexusworldwide.com>
2019-05-13 23:15:38 -0700

    reverted code

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,19 +1,17 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { Hero }         from '../hero';
 import { HeroService }  from '../hero.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: [ './hero-detail.component.css' ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: [ './hero-detail.component.css' ]
 })
 export class HeroDetailComponent implements OnInit {
-  // hero: Hero;
+  hero: Hero;
 
   constructor(
     private route: ActivatedRoute,
@@ -22,22 +20,21 @@ export class HeroDetailComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    // this.getHero();
+    this.getHero();
   }
 
-  getHero(): Observable<any> {
-    console.log("get herro is called.")
+  getHero(): void {
     const id = +this.route.snapshot.paramMap.get('id');
-    return this.heroService.getHero(id)
-      // .subscribe(hero => this.hero = hero);
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
   }
 
   goBack(): void {
     this.location.back();
   }
 
- save(hero): void {
-    this.heroService.updateHero(hero)
+ save(): void {
+    this.heroService.updateHero(this.hero)
       .subscribe(() => this.goBack());
   }
 }
